03dae404236d1abe58b3e9fbe57bcbfe63b02cc1
Gabriel Rubens <grsabreu@gmail.com>
2016-09-04 14:48:35 -0300

    Link react-native-maps

--- android/app/src/main/java/com/astroapp/MainApplication.java
+++ android/app/src/main/java/com/astroapp/MainApplication.java
@@ -4,9 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
-import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,9 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new VectorIconsPackage(),
-            new RNSpinkitPackage(),
-            new ReactNativeOneSignalPackage()
+            new RNSpinkitPackage()
       );
     }
   };
