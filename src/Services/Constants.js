05b994cde8ff732350d6d8e52c137c1b45326e75
Gabriel Rubens <grsabreu@gmail.com>
2016-09-11 11:48:04 -0300

    Minor changes on Constants

--- src/Services/Constants.js
+++ src/Services/Constants.js
@@ -1,5 +1,7 @@
+const SERVER_ADDRESS = __DEV__ ? '127.0.0.1' : '104.131.12.202'
+
 export default {
-  METEOR_SERVER: 'ws://127.0.0.1:3000/websocket',
+  METEOR_SERVER: `ws://${SERVER_ADDRESS}:3000/websocket`,
   CLOUDINARY_API_SECRET: 'abcfalsjfal',
   CLOUDINARY_API_KEY: '000000000000000',
   CLOUDINARY_CLOUD: 'astrosomething',
