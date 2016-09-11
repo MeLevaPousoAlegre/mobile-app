de4d5b78ddb04604df2390b42644015d45c55be5
Gabriel Rubens <grsabreu@gmail.com>
2016-09-11 12:21:10 -0300

    Fix URL

--- src/Services/Constants.js
+++ src/Services/Constants.js
@@ -1,7 +1,8 @@
-const SERVER_ADDRESS = __DEV__ ? '127.0.0.1' : '104.131.12.202'
+const METEOR_SERVER = __DEV__ ? 'ws://127.0.0.1:3000/websocket' : 'ws://104.131.12.202/websocket'
 
+console.warn('Will attempt to connect ws on', METEOR_SERVER)
 export default {
-  METEOR_SERVER: `ws://${SERVER_ADDRESS}:3000/websocket`,
+  METEOR_SERVER,
   CLOUDINARY_API_SECRET: 'abcfalsjfal',
   CLOUDINARY_API_KEY: '000000000000000',
   CLOUDINARY_CLOUD: 'astrosomething',
