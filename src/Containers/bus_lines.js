107c8bb02b1b4b99c8e8b5fbc3b000f47afed8b2
Gabriel Rubens <grsabreu@gmail.com>
2016-09-11 11:47:34 -0300

    Create a better container for buslines

--- src/Containers/bus_lines.js
+++ src/Containers/bus_lines.js
@@ -1,19 +1,42 @@
-import composeWithTracker from 'rn-meteor-containerize';
-import Meteor from 'react-native-meteor';
+import { compose } from 'react-komposer'
+import Meteor from 'react-native-meteor'
+import EJSON from 'ejson'
+import {
+  AsyncStorage,
+  View,
+} from 'react-native'
+import _ from 'lodash'
+
+const BUS_LINE_STORAGE_KEY = '@appData/busLines'
 
 function composer(props, onData){
-  const handle = Meteor.subscribe('busLines.all');
+  AsyncStorage.getItem(BUS_LINE_STORAGE_KEY, (readError, storedBusLines) => {
+    if(readError) return console.warn(readError)
 
-  if(handle.ready()){
-    onData(null, {
-      busLines: Meteor.collection('busLines').find({}) || [],
-    })
-  } else {
-    onData(null, {
-      loading: true,
-      busLines: [],
-    })
-  }
+    if(_.isEmpty(storedBusLines))
+      Meteor.call('busLines/fetchAll', (error, busLines) => {
+        if(error) return console.warn('Error', error)
+
+        console.warn(`Read ${busLines.length} bus lines`)
+        AsyncStorage.setItem(BUS_LINE_STORAGE_KEY, EJSON.stringify(busLines))
+        onData(null, {
+          busLines,
+          loading: false,
+        })
+      })
+    else {
+      console.warn('Read bus lines from storage')
+      onData(null, {
+        busLines: EJSON.parse(storedBusLines),
+        loading: false,
+      })
+    }
+  })
+
+  onData(null, {
+    busLines: [],
+    loading: true,
+  })
 }
 
-export default composeWithTracker(composer);
+export default compose(composer, () => <View/>, () => <View/>);
